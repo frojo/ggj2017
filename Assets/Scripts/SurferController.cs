e287da788299e6473ad097021d5ec2460ebf19d8
Francisco Rojo <mojoz0101@gmail.com>
2017-01-21 00:09:44 -0500

    Initial island and surfer with movement

---
+++ Assets/Scripts/SurferController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SurferController : MonoBehaviour {
+
+	public GameObject island;
+	private Vector2 island_position;
+
+	// How fast the surfer approaches the island
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		island_position = island.transform.position;
+
+
+		// TODO: Find the island
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = 
+			Vector2.MoveTowards (transform.position, island_position, speed * Time.deltaTime);
+		
+	
+	}
+}
