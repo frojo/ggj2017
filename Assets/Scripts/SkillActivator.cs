25d8b74bd3026be77465ca0904ba01ede25842ab
Francisco Rojo <mojoz0101@gmail.com>
2017-01-21 15:36:08 -0500

    Dolphins launching in main woo

--- Assets/Scripts/SkillActivator.cs
+++ Assets/Scripts/SkillActivator.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class SkillActivator : MonoBehaviour {
 
     //public Transform skill;
+	public Camera main_camera;
+	public GameObject pusher;
 
     void Start () {
     
@@ -13,7 +15,19 @@ public class SkillActivator : MonoBehaviour {
         if (Input.GetMouseButtonUp(0))
         {
             Debug.Log("Launching skill!");
+			InstantiatePusher (
+				main_camera.ScreenToWorldPoint (Input.mousePosition));
             gameObject.SetActive(false);
         }
     }
+
+	void InstantiatePusher(Vector2 mouse_world_position) {
+
+		Vector2 start_point = transform.position;
+		pusher.GetComponent<Pusher>().start_point = start_point;
+		pusher.GetComponent<Pusher> ().direction = 
+			(mouse_world_position - start_point).normalized;
+
+		Instantiate (pusher);
+	}
 }
