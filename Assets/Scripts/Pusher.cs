99ea2844b956e45c21981fee9bf04b91a14fcaf6
Francisco Rojo <mojoz0101@gmail.com>
2017-01-21 11:58:53 -0500

    Pusher is correct rotation

--- Assets/Scripts/Pusher.cs
+++ Assets/Scripts/Pusher.cs
@@ -10,9 +10,16 @@ public class Pusher : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		start_point = transform.position;
+
+		// Rotate dolphin so that it goes in straight line
+
+		Vector2 difference = end_point - start_point;
+		float sign = (end_point.x < start_point.x)? 1.0f : -1.0f;
+		float angle = Vector2.Angle(Vector2.up, difference);
+		transform.Rotate (Vector3.forward*angle*sign);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		// Move towards end point
@@ -29,6 +36,12 @@ public class Pusher : MonoBehaviour {
 		if (other.tag == "Enemy") {
 			other.GetComponent<Surfer>().InitiatePush(end_point, speed);
 		}
+	}
+
+	// Call this to properly initialize the pusher
+	void Init(Vector2 click_coordinates) {
+		
+		
 
 	}
 }
