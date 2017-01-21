e303e5263694688a4864efb5335a2638bb41f37b
Francisco Rojo <mojoz0101@gmail.com>
2017-01-21 10:50:50 -0500

    Pusher actually pushes now

--- Assets/Scripts/Pusher.cs
+++ Assets/Scripts/Pusher.cs
@@ -11,18 +11,24 @@ public class Pusher : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		// Move towards end point
 		transform.position = 
 			Vector2.MoveTowards (transform.position, end_point, speed * Time.deltaTime);
-		
 
+		// Disappear once it gets to the end
 		if (((Vector2)transform.position) == end_point) {
 			Destroy (gameObject);
 		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Enemy") {
+			other.GetComponent<Surfer>().InitiatePush(end_point, speed);
+		}
+
 	}
 }
--- Assets/Scripts/Surfer.cs
+++ Assets/Scripts/Surfer.cs
@@ -3,26 +3,40 @@ using System.Collections;
 
 public class Surfer : MonoBehaviour {
 
+	// Island that the surfer is approaching
 	public GameObject island;
-	private Vector2 island_position;
+	Vector2 island_position;
 
 	// How fast the surfer approaches the island
-	public float speed;
+	public float surf_speed;
+
+	Vector2 end_point;
+	float speed;
+
+	float push_speed;
 
 	// Use this for initialization
 	void Start () {
 		island_position = island.transform.position;
-
-
-		// TODO: Find the island
-	
+		end_point = island_position;
+		speed = surf_speed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.position = 
-			Vector2.MoveTowards (transform.position, island_position, speed * Time.deltaTime);
-		
-	
+			Vector2.MoveTowards (transform.position, end_point, speed * Time.deltaTime);
+	}
+
+	// Pushers/weapons call this to push the surfer away
+	public void InitiatePush (Vector2 push_destination, float push_speed) {
+		end_point = push_destination;
+		speed = push_speed;
+	}
+
+	// Pushers/weapons call this to end the push
+	public void EndPush () {
+		end_point = island_position;
+		speed = surf_speed;
 	}
 }
