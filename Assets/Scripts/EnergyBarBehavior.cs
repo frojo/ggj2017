e998b36dac2a31f84b3832ce07ff8ae5a4732541
Francis Joseph Serina <francis.serina@gmail.com>
2017-01-21 11:50:23 -0500

    + Energy Bar + UI Skill Buttons

---
+++ Assets/Scripts/EnergyBarBehavior.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnergyBarBehavior : MonoBehaviour {
+
+    public float maxEnergy = 100;
+    public float energyGainRate = 5; // per sec
+    public float currentEnergy { get; private set; }
+
+    private Vector2 maxDimension;
+    private Vector3 localScale;
+
+    private RectTransform rectTransform;
+
+    public static EnergyBarBehavior instance { get; private set; }
+
+    void Awake ()
+    {
+        instance = this;
+    }
+
+    void OnDestroy()
+    {
+        instance = null;
+    }
+
+    void Start () {
+        rectTransform = GetComponent<RectTransform>();
+
+        maxDimension = rectTransform.sizeDelta;
+        localScale = rectTransform.sizeDelta;
+        localScale.x = 0;
+        rectTransform.sizeDelta = localScale;
+
+        currentEnergy = 0;
+    }
+    
+    void Update () {
+        IncreaseEnergy(Time.deltaTime);
+        UpdateVisual();
+    }
+
+    void IncreaseEnergy(float dt)
+    {
+        currentEnergy += energyGainRate * dt;
+        currentEnergy = Mathf.Clamp(currentEnergy, 0, maxEnergy);
+    }
+
+    void UpdateVisual()
+    {
+        localScale.x = maxDimension.x * currentEnergy / maxEnergy;
+        rectTransform.sizeDelta = localScale;
+    }
+
+    public bool ConsumeEnergy(float amount)
+    {
+        if (currentEnergy < amount)
+        {
+            return false;
+        }
+
+        currentEnergy -= amount;
+        return true;
+    }
+}
