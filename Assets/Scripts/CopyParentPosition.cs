bc1c88a3e601bb003a86c3ed210213f051235ddd
Francis Joseph Serina <francis.serina@gmail.com>
2017-01-21 16:44:14 -0500

    + animation angle selector

---
+++ Assets/Scripts/CopyParentPosition.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CopyParentPosition : MonoBehaviour {
+
+    private Transform parent;
+
+    void Start () {
+        parent = transform.parent;
+        transform.parent = null;
+
+        var angles = transform.eulerAngles;
+        angles.z = 0;
+        transform.eulerAngles = angles;
+    }
+    
+    void LateUpdate () {
+        if (parent)
+        {
+            transform.position = parent.position;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/SkillActivator.cs
+++ Assets/Scripts/SkillActivator.cs
@@ -3,9 +3,7 @@ using System.Collections;
 
 public class SkillActivator : MonoBehaviour {
 
-    //public Transform skill;
-	public Camera main_camera;
-	public GameObject pusher;
+    public GameObject pusher;
 
     void Start () {
     
@@ -14,20 +12,19 @@ public class SkillActivator : MonoBehaviour {
     void Update () {
         if (Input.GetMouseButtonUp(0))
         {
-            Debug.Log("Launching skill!");
-			InstantiatePusher (
-				main_camera.ScreenToWorldPoint (Input.mousePosition));
+            InstantiatePusher (
+                Camera.main.ScreenToWorldPoint (Input.mousePosition));
             gameObject.SetActive(false);
         }
     }
 
-	void InstantiatePusher(Vector2 mouse_world_position) {
+    void InstantiatePusher(Vector2 mouse_world_position) {
 
-		Vector2 start_point = transform.position;
-		pusher.GetComponent<Pusher>().start_point = start_point;
-		pusher.GetComponent<Pusher> ().direction = 
-			(mouse_world_position - start_point).normalized;
+        Vector2 start_point = transform.position;
+        pusher.GetComponent<Pusher>().start_point = start_point;
+        pusher.GetComponent<Pusher> ().direction = 
+            (mouse_world_position - start_point).normalized;
 
-		Instantiate (pusher);
-	}
+        Instantiate (pusher);
+    }
 }
