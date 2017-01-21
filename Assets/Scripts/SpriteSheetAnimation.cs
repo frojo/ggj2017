da33b6d0b3a98fd940329a9c4637073842b6ec76
Francis Joseph Serina <francis.serina@gmail.com>
2017-01-21 14:23:12 -0500

    + dolphin sprites + sprite sheet animation ~ applied UI to main

---
+++ Assets/Scripts/SpriteSheetAnimation.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteSheetAnimation : MonoBehaviour {
+
+    public Sprite[] sprites;
+    public float fps = 30;
+    private float frameDuration;
+    private float timeSinceLastChange;
+    private SpriteRenderer spriteRenderer;
+    private int currentIndex = 0;
+
+    void Start () {
+        Debug.Assert(sprites.Length > 1, "sprites not set", this);
+        frameDuration = 1.0f / fps;
+        timeSinceLastChange = Time.time;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprites[currentIndex];
+    }
+    
+    void Update () {
+        if (Time.time > timeSinceLastChange + frameDuration)
+        {
+            IncrementIndex();
+            UpdateVisual();
+            timeSinceLastChange = Time.time;
+        }
+    }
+
+    void IncrementIndex()
+    {
+        ++currentIndex;
+        if (currentIndex >= sprites.Length)
+        {
+            currentIndex = 0;
+        }
+    }
+
+    void UpdateVisual()
+    {
+        spriteRenderer.sprite = sprites[currentIndex];
+    }
+}
